73c3eaea572205a8d58100ebc504d67a131caeec
Seoyoung Lee <lsyhime@snu.ac.kr>
2022-09-18 23:51:06 +0900

    feat(car): impl Car data class
    1. validate car name with compainon object fun
    2. move forward method that updates distance
    3. override toString for pretty print

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,1 +1,17 @@
-class Car {}
+import kotlin.random.Random
+
+data class Car(val name: String, var distance: Int = 0) {
+    companion object {
+        fun validCarName(suspiciousName: String): Boolean {
+            return suspiciousName.length <= 5
+        }
+    }
+
+    fun moveForward(distance: Int) {
+        this.distance += distance
+    }
+
+    override fun toString(): String {
+        return "{$name} : {$distance}"
+    }
+}
