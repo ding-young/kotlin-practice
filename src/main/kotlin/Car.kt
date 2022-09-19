f6a9cce9e4a4e2927afc0796dcf0a8985ddac6c3
Seoyoung Lee <lsyhime@snu.ac.kr>
2022-09-19 22:54:27 +0900

    feat(car): update moveForward logic
    1. moveForward increases car's distance by 1 when cond mets
    2. add threshold field

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,15 +1,13 @@
-import kotlin.random.Random
-
 data class Car(val name: String, var distance: Int = 0) {
+    private val threshold = 4
+
     companion object {
         fun validCarName(suspiciousName: String): Boolean {
             return suspiciousName.length <= 5
         }
     }
 
-    fun moveForward(distance: Int) {
-        this.distance += distance
-    }
+    fun moveForward(inputNum: Int) = this.let { if (inputNum >= threshold) it.distance += 1 }
 
     override fun toString(): String {
         return "{$name} : {$distance}"
